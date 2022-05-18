9700da03eadbbd42f387b45335d65c522531c974
pumb3r <mishakvochko@ukr.net>
2022-05-18 16:05:32 +0300

    Lesson 9 Task1

---
+++ src/lesson/lesson9/index.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+import TableOfUsers from './TableOfUsers'
+
+const USERS = [
+  {
+    id: 1,
+    firstName: 'David',
+    lastName: 'Dubliakov',
+    age: '19'
+  },
+  {
+    id: 2,
+    firstName: 'Dubliakov',
+    lastName: 'David',
+    age: '21'
+  },
+  {
+    id: 3,
+    firstName: 'Bohdan',
+    lastName: 'Semenov',
+    age: '19'
+  },
+  {
+    id: 4,
+    firstName: 'Semenov',
+    lastName: 'Bohdan',
+    age: '30'
+  },
+  {
+    id: 5,
+    firstName: 'Misha',
+    lastName: 'Kvochko',
+    age: '20'
+  },
+  {
+    id: 6,
+    firstName: 'Kvochko',
+    lastName: 'Misha',
+    age: '11'
+  },
+]
+
+const Main = () => {
+
+  const [users, setUsers] = useState(USERS)
+
+  return (
+    <div>
+      <h1>Table of users</h1>
+      <TableOfUsers data={users} />
+    </div>
+  );
+};
+
+export default Main;

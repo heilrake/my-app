37ab92b9cdeded9814063cccbf0ffa26c248f70c
Миша <58284931+pumb3r@users.noreply.github.com>
2022-05-19 14:17:06 +0300

    [FEAT] Lesson-10-task-4 (#6)
    * adds task4
    
    * fixed lines links

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import './App.css';
 
+<<<<<<< HEAD
+import CurrentTask from './lesson/Lesson10/Task4';
+=======
 import CurrentTask from './lesson/Lesson10/Task5';
+>>>>>>> daf478780e5d18964db4075432e24888cbaa6ecd
 
 const App = () => {
   return (
---
+++ src/lesson/Lesson10/Task4/component/TableOfUsers.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Link,
+} from '@mui/material';
+
+const TableOfUsers = ({ users, showMessage }) => (
+  <div>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Name</TableCell>
+          <TableCell>Last name</TableCell>
+          <TableCell colspan='2'>Age</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {users.map((item, index) => (
+          <TableRow key={index}>
+            <TableCell>{item.firstName}</TableCell>
+            <TableCell>{item.lastName}</TableCell>
+            <TableCell>{item.age}</TableCell>
+            <TableCell>
+              <Link href='#' onClick={() => showMessage(item.id)}>Click me!</Link >
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </div>
+);
+
+export default TableOfUsers;

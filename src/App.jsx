8341e4b5941a8e4d2b74b96def8a4af7190687db
Миша <58284931+pumb3r@users.noreply.github.com>
2022-05-23 12:21:03 +0300

    [FEAT]lesson-10-task-8 (#12)
    [FEAT]lesson-10-task-8 (#12)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import './App.css';
 
-<<<<<<< HEAD
-import CurrentTask from './lesson/Lesson10/Task4';
-=======
-import CurrentTask from './lesson/Lesson10/Task5';
->>>>>>> daf478780e5d18964db4075432e24888cbaa6ecd
+import CurrentTask from './lesson/Lesson10/Task8';
 
 const App = () => {
   return (

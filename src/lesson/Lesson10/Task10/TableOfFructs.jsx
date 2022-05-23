a646aba8f5338ca9e62f377439e727d46b4f2b8d
pumb3r <mishakvochko@ukr.net>
2022-05-23 12:46:48 +0300

    test

---
+++ src/lesson/Lesson10/Task10/TableOfFructs.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Button,
+  Checkbox,
+} from '@mui/material';
+
+import useStyles from './style/style';
+
+const TableOfFructs = ({ fruits, deleteElement }) => {
+  const classes = useStyles();
+
+  return (
+    <Table className={classes.root}>
+      <TableHead>
+        <TableRow>
+          <TableCell colSpan={2}>Name</TableCell>
+
+          <TableCell>Count</TableCell>
+          <TableCell>Price of one</TableCell>
+          <TableCell colSpan={0}>Price of all</TableCell>
+          <TableCell></TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {fruits.map((item, index) => (
+          <TableRow key={index}>
+            <TableCell><Checkbox /></TableCell>
+            <TableCell>{item.name}</TableCell>
+            <TableCell>{item.count}</TableCell>
+            <TableCell>{item.priceOfOne}</TableCell>
+            <TableCell>{item.priceOfOne * item.count}</TableCell>
+            <TableCell>
+              <Button onClick={() => deleteElement(index)} variant="contained">Delete</Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default TableOfFructs;

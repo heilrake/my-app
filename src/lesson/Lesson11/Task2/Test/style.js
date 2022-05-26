6109f6a3871a85a350f0ae235e6cc9748102326b
pumb3r <mishakvochko@ukr.net>
2022-05-26 09:59:15 +0300

    adds task 2

---
+++ src/lesson/Lesson11/Task2/Test/style.js
@@ -0,0 +1,22 @@
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+  root: {
+    margin: '50px 0px',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+    height: '100%',
+    fontSize: '24px',
+
+    "& .red ": {
+      color: 'red',
+    },
+    "& .green": {
+      color: 'green',
+    },
+
+  },
+});
+export default useStyles;

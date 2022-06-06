3c9daa6320ac925b9e9547a3eda541f540461bde
pumb3r <mishakvochko@ukr.net>
2022-06-06 15:03:31 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import './App.css';
 
-<<<<<<< HEAD
 import CurrentTask from './lesson/Lesson11/Task6';
-
-=======
-import CurrentTask from './lesson/Lesson11/Task5';
->>>>>>> 4c65206208eb0a25fccc00e3ab922d5e4415f55a
 
 const App = () => {
   return (
